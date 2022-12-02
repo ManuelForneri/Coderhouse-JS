711d4ecc101bee2d1ddfd24e31559b0f21f22adb
manuel forneri <manuelforneri@hotmail.com>
2022-12-02 17:05:18 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-let edad = prompt("ingrese la edad de la persona, para salir ingrese X");
 let personas = 0;
 let suma = 0;
 
+let edad = prompt("ingrese la edad de la persona, para salir ingrese X");
+
 while (edad != "X") {
   if (edad == "X") {
-    console.log("Salio");
-    console.log(personas);
-    console.log(suma);
   } else if ((edad > 0) & (edad < 110)) {
     personas++;
-    suma = suma + parseInt(edad);
+    suma = suma + parseFloat(edad);
+  } else {
+    alert("Numero fuera de rango de edad ");
   }
   edad = prompt("ingrese la edad de la persona, para salir ingrese X");
 }
 
 let promedio = suma / personas;
-alert("El promedio de edad es :" + promedio);
+alert("El promedio de edad es : " + promedio);
