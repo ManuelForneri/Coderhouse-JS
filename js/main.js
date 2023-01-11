6f49c67e4e56ae41d1b3b246f3539732dfb42c37
ManuelForneri <manuelforneri@hotmail.com>
2023-01-11 18:24:35 -0300

    update products

--- js/main.js
+++ js/main.js
@@ -1,122 +1,21 @@
-/* const products = [];
-const services = [];
+const products = [];
 // Se creo un constructor de objetos de productos
 
 class Producto {
-  constructor(titulo, precio, marca, color) {
+  constructor(titulo, precio) {
     this.id = products.length + 1;
     this.titulo = titulo;
     this.precio = precio;
-    this.marca = marca;
-    this.color = color;
   }
 }
-products.push(new Producto("Reloj Caballero", 2000, "Lemon", "Negro"));
-products.push(new Producto("Reloj Dama", 2500, "Casio", "Rosa"));
-products.push(new Producto("Mate imperial", 5500, "MateCampo", "Negro"));
+products.push(new Producto("Mountain bike Battle 210 R29", 80000));
+products.push(new Producto("Bicicleta Silverfox 2021 M", 75689));
+products.push(new Producto("Mountain Bike Thunder Sky Rodado 29", 66799));
+products.push(new Producto("Mountain bike TopMega Sunshine R29", 86899));
+products.push(new Producto("Mountain bike Shifter", 64999));
+products.push(new Producto("Mountain bike SLP 5 pro R29 18", 79376));
+products.push(new Producto("Bicicleta paseo femenina Futura", 51838));
+products.push(new Producto("Mountain bike Olmo Wish 290", 94499));
+products.push(new Producto("Bicicleta plegable Fire Bird R20", 85000));
 
-// Se creo un constructor de objetos de servicios
-class Servicio {
-  constructor(tipo, costo, descripcion) {
-    this.id = services.length + 1;
-    this.tipo = tipo;
-    this.costo = costo;
-    this.descripcion = descripcion;
-  }
-}
-services.push(
-  new Servicio("compostura", 2000, "todo tipo de composturas     ")
-);
-services.push(
-  new Servicio("Reparacion de joyas", 5000, "Reparacion de todo tipo de joyas")
-);
-services.push(
-  new Servicio(
-    "reparacion de relojes",
-    6000,
-    "Reparacion de todo tipo de relojes"
-  )
-);
-
-function addProducto() {
-  let titulo = prompt("Ingrese el titulo del producto");
-  let precio = parseFloat(prompt("Ingrese el precio del producto"));
-  while (isNaN(precio)) {
-    precio = parseFloat(
-      prompt("Error, eso no es un numero, intentelo otra vez :")
-    );
-  }
-  let marca = prompt("Ingrese la marca del producto");
-  let color = prompt("Ingrese el color del producto");
-  products.push(new Producto(titulo, precio, marca, color));
-  console.table(products);
-}
-function addServices() {
-  let tipo = prompt("Ingrese el nombre del servicio");
-  let precio = parseFloat(prompt("Ingrese el precio del Servicio"));
-  while (isNaN(precio)) {
-    precio = parseFloat(
-      prompt("Error, eso no es un numero, intentelo otra vez :")
-    );
-  }
-  let descripcion = prompt("Ingrese una descripcion del servicio");
-  services.push(new Servicio(tipo, precio, descripcion));
-  console.table(services);
-}
-
-function searchProduct(products) {
-  let busqueda = prompt("Ingrese un producto de nuestro catalogo ");
-  const resultadoBusqueda = products.find((el) => {
-    return el.titulo.includes(busqueda);
-  });
-  if (resultadoBusqueda == false) {
-    alert(
-      "No se encontro ningun producto con ese nombre, por favor intentelo de nuevo"
-    );
-  } else {
-    console.log("Resultado de la busqueda");
-    console.table(resultadoBusqueda);
-  }
-}
-function searchService(services) {
-  let busquedaServicio = prompt("Ingrese un servicio de nuestro local");
-  const resultado = services.find((el) => {
-    return el.tipo === busquedaServicio;
-  });
-  if (resultado == undefined) {
-    alert(
-      "No se encontro ningun servicio con ese nombre, por favor intentelo de nuevo"
-    );
-  } else {
-    console.log("Resultado de la busqueda de servicios");
-    console.table(resultado);
-  }
-}
-let opcion = prompt(
-  "Ingrese la opcion \n 1. Agregar un producto \n 2. Agregar un Servicio \n 3 Buscar un producto \n 4. Buscar un servicio\n X. Salir"
-);
-while (opcion != "X") {
-  switch (opcion) {
-    case "1":
-      addProducto();
-      break;
-    case "2":
-      addServices();
-      break;
-    case "3":
-      searchProduct(products);
-      break;
-    case "4":
-      searchService(services);
-      break;
-    case "X":
-      break;
-    default:
-      alert("No se encontro la opcion que ingreso, vuelva a intentarlo");
-      break;
-  }
-  opcion = prompt(
-    "Ingrese la opcion \n 1. Agregar un producto \n 2. Agregar un Servicio \n 3 Buscar un producto \n 4. Buscar un servicio\n X. Salir "
-  );
-}
- */
+console.table(products);
