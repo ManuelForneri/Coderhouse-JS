af91da9253842feb81e97a9dd6b092c69b8b1c92
ManuelForneri <manuelforneri@hotmail.com>
2023-01-19 12:18:20 -0300

    Se actualizo la creacion de cards automaticamente

--- js/main.js
+++ js/main.js
@@ -5,21 +5,79 @@ const shoppingCart = [];
 /* Crea un nuevo objeto de producto con una identificación única, un título y un precio. */
 
 class Producto {
-  constructor(titulo, precio) {
+  constructor(titulo, precio, image) {
     this.id = products.length + 1;
     this.titulo = titulo;
     this.precio = precio;
+    this.image = image;
   }
 }
-products.push(new Producto("Mountain bike Battle 210 R29", 80000));
-products.push(new Producto("Bicicleta Silverfox 2021 M", 75689));
-products.push(new Producto("Mountain Bike Thunder Sky Rodado 29", 66799));
-products.push(new Producto("Mountain bike TopMega Sunshine R29", 86899));
-products.push(new Producto("Mountain bike Shifter", 64999));
-products.push(new Producto("Mountain bike SLP 5 pro R29 18", 79376));
-products.push(new Producto("Bicicleta paseo femenina Futura", 51838));
-products.push(new Producto("Mountain bike Olmo Wish 290", 94499));
-products.push(new Producto("Bicicleta plegable Fire Bird R20", 85000));
+products.push(
+  new Producto(
+    "Mountain bike Battle 210 R29",
+    80000,
+    "../assets/mountainBike210.webp"
+  )
+);
+products.push(
+  new Producto("Bicicleta Silverfox 2021 M", 75689, "../assets/silverFox.webp")
+);
+products.push(
+  new Producto(
+    "Mountain Bike Thunder Sky Rodado 29",
+    66799,
+    "../assets/MountainBikeThunderSky.webp"
+  )
+);
+products.push(
+  new Producto(
+    "Mountain bike TopMega Sunshine R29",
+    86899,
+    "../assets/mountainBikeTopMega.webp"
+  )
+);
+products.push(
+  new Producto(
+    "Mountain bike Shifter",
+    64999,
+    "../assets/MountainBikeShifter.webp"
+  )
+);
+products.push(
+  new Producto(
+    "Mountain bike SLP 5 pro R29 18",
+    79376,
+    "../assets/MountainbikeSLP5pro.webp"
+  )
+);
+products.push(
+  new Producto(
+    "Bicicleta paseo femenina Futura",
+    51838,
+    "../assets/BicicletaFuturaCountryR26.webp"
+  )
+);
+products.push(
+  new Producto(
+    "Mountain bike Olmo Wish 290",
+    94499,
+    "../assets/MountainbikeOlmoWish290.webp"
+  )
+);
+products.push(
+  new Producto(
+    "Bicicleta plegable Fire Bird R20",
+    85000,
+    "../assets/BicicletaPlegableFireBirdR20.webp"
+  )
+);
+products.push(
+  new Producto(
+    "Bicicleta Ghepard X 2.0 Mtb R29 Shimano Aluminio Slp Disco",
+    88.916,
+    "../assets/BicicletaGhepardX2.0MtbR29.webp"
+  )
+);
 
 console.table(products);
 
@@ -28,10 +86,22 @@ elementos con el nombre de clase "título-producto" en el título del producto y
 los elementos con el nombre de clase "precio-producto" en el precio del producto. */
 
 for (i = 0; i < products.length; i++) {
-  let titulos = document.getElementsByClassName("title-product");
-  let precio = document.getElementsByClassName("product-price");
-  titulos[i].innerText = products[i].titulo;
-  precio[i].innerText = "$" + products[i].precio;
+  let contenedorProductos = document.querySelector(".container-grid-products");
+
+  let newCard = `
+  <div class="product-card animated fadeIn">
+  <img src="${products[i].image}" alt="" class="imagenes" />
+  <p class="title-product">${products[i].titulo}</p>
+  <p class="product-price">${products[i].precio}</p>
+  <input
+    id="button-${products[i].id}"
+    type="button"
+    class="btn-compra"
+    value="Comprar"
+    onclick="addShoppingCart(${products[i].id})"
+  />
+</div>`;
+  contenedorProductos.innerHTML += newCard;
 }
 
 /* Obtener el elemento con el id "anio" y establecer el texto interno en el año actual. */
@@ -60,7 +130,7 @@ function addShoppingCart(id) {
     icon: "success",
     title: "su producto se añadio al carrito",
     showConfirmButton: false,
-    timer: 1501,
+    timer: 1500,
     backdrop: false,
   });
 }
