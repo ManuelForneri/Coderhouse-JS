77e742264a9896bc2a5613a311000dc3b14f0de0
ManuelForneri <manuelforneri@hotmail.com>
2023-01-26 12:35:44 -0300

    Update shoppingCart.js

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,9 +1,11 @@
+/* Esto es obtener el almacenamiento local y establecer el total en 0. */
 let shop = JSON.parse(localStorage.getItem("Carrito de Compras"));
 let total = document.getElementById("total");
 let t = 0;
+const contenedorCarrito = document.querySelector(".container-shoppingCart");
 
+/* Creando un nuevo div para cada artículo en el carrito de compras. */
 for (i = 0; i < shop.length; i++) {
-  let contenedorCarrito = document.querySelector(".container-shoppingCart");
   t += shop[i].precio;
   let newShop = ` <div class="shop-card animated fadeIn">
         <div class="middle">
@@ -17,7 +19,7 @@ for (i = 0; i < shop.length; i++) {
                 type="button"
                 class="btn-eliminar"
                 value="Eliminar"
-                onclick='eliminarProducto(${shop[i].id})'
+                onclick='eliminarProducto(${i})'
             />
         </div>
     </div>
@@ -25,6 +27,10 @@ for (i = 0; i < shop.length; i++) {
   contenedorCarrito.innerHTML += newShop;
 }
 total.innerText = `Total : $${t}`;
+
+/**
+ * Limpia el almacenamiento local y el carrito de compras.
+ */
 function vaciar() {
   Swal.fire({
     title: "Seguro que quiere borrar todos los productos del carrito?",
@@ -39,12 +45,16 @@ function vaciar() {
     if (result.isConfirmed) {
       Swal.fire("Deleted!", "Your file has been deleted.", "success");
       localStorage.clear();
-      let contenedorCarrito = document.querySelector(".container-shoppingCart");
       contenedorCarrito.innerHTML = "";
       total.innerText = "Total : $0";
     }
   });
 }
+/**
+ * Es una función que borra el almacenamiento local y el carrito de compras cuando el usuario hace clic
+ * en el botón "comprar".
+ */
+
 function comprar() {
   Swal.fire({
     title: "Quiere comprar estos productos?",
@@ -68,7 +78,8 @@ function comprar() {
     }
   });
 }
-function eliminarProducto(id) {
+
+function eliminarProducto(posicion) {
   let newArr = [];
   Swal.fire({
     title: "Seguro que quiere eliminar el producto?",
@@ -86,12 +97,42 @@ function eliminarProducto(id) {
         "su producto se elimino correctamente.",
         "success"
       );
+      contenedorCarrito.innerHTML = "";
       for (i = 0; i < shop.length; i++) {
-        if (shop[i].id != id) {
-          newArr.push(shop[i]);
+        if (shop.length > 1) {
+          if (i != posicion) {
+            newArr.push(shop[i]);
+          }
+        } else {
+          if (i == posicion) {
+            newArr = [];
+          }
         }
       }
       localStorage.setItem("Carrito de Compras", JSON.stringify(newArr));
+      t = 0;
+      for (i = 0; i < newArr.length; i++) {
+        t += newArr[i].precio;
+        let newShop = ` <div class="shop-card animated fadeIn">
+              <div class="middle">
+                  <img src="${newArr[i].image}" alt="" class="imagenes" />
+              </div>
+              <div class="middle">
+                  <p class="title-shop">${newArr[i].titulo}</p>
+                  <p class="product-price-shop">$${newArr[i].precio}</p>
+                  <input
+                      id="button-${newArr[i].id}"
+                      type="button"
+                      class="btn-eliminar"
+                      value="Eliminar"
+                      onclick='eliminarProducto(${i})'
+                  />
+              </div>
+          </div>
+            `;
+        contenedorCarrito.innerHTML += newShop;
+      }
+      total.innerText = `Total : $${t}`;
     }
   });
 }
